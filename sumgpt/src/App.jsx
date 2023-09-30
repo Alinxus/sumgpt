42bd87a9caf8036ead4a40ac99a969b17584c84f
Alinxus <81990934+Alinxus@users.noreply.github.com>
2023-09-30 20:32:33 +0000

    added google analytics

--- sumgpt/src/App.jsx
+++ sumgpt/src/App.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import Hero from './components/Hero'
 import Demo from './components/Demo'
 import './App.css'
+import ReactGA from 'react-analytics'
+import { Helmet } from "react-helmet"
+
 
 
 const App = () => {
   return (
+    <>
+    <Helmet>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-24E564EY06"></script>
+      <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'G-24E564EY06');
+    </script>
+    </Helmet>
     <main>
         <div className="main">
             <div className="gradient" />
@@ -15,6 +29,7 @@ const App = () => {
             <Demo />
         </div>
     </main>
+    </>
   )
 }
 
