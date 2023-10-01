e07abed783f63ed2e4d469e50a42e590740bb7f7
Alinxus <81990934+Alinxus@users.noreply.github.com>
2023-10-01 07:23:58 +0000

    authentication added

--- sumgpt/src/App.jsx
+++ sumgpt/src/App.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Demo from './components/Demo'
+import Login from './Login'
+import { useAuth0 } from '@auth0/auth0-react';
+
 import './App.css'
-import ReactGA from 'react-analytics'
-import { Helmet } from "react-helmet"
 
 
-
-const App = () => {
-  return (
-    <>
-    <head>
-    <Helmet>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-F5E92MCQSE"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)};
-  gtag('js', new Date());
-
-  gtag('config', 'G-F5E92MCQSE');
-</script>
-
-    </Helmet>
-    </head>
-    <main>
+const App = () => { 
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
+    useAuth0();
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+    if (error) {
+      return <div>Oops... {error.message}</div>;
+    }
+  
+    if (isAuthenticated) {
+      return (
+        <main>
         <div className="main">
             <div className="gradient" />
         </div>
@@ -32,8 +28,13 @@ const App = () => {
             <Demo />
         </div>
     </main>
-    </>
-  )
-}
+
+      );
+    } else {
+      return <button onClick={() => loginWithRedirect()}>Log in</button>;
+    }
+  }
+
+
 
 export default App
--- sumgpt/src/main.jsx
+++ sumgpt/src/main.jsx
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
 import { store } from './services/store';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+    domain="dev-rbrdusjbcya4c4so.us.auth0.com"
+    clientId="BdTEX9eqlyyPzIInclEvAuWOuQqItJ6u"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
+  </Auth0Provider>
 )
